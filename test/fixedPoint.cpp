91535a031f1d99241ba1d1051c4275677d8d4224
technik <cjtech90@gmail.com>
2018-05-08 21:27:41 +0200

    Test with candidate syntax.

--- test/fixedPoint.cpp
+++ test/fixedPoint.cpp
@@ -2,6 +2,7 @@
 // Animation system unit test
 //-----------------------------------------------------------------------------
 
+#include <cassert>
 #include <math/fixedPoint.h>
 
 using Real = etl::math::UFixed<11,5>;
@@ -10,5 +11,8 @@ int main()
 {
 	Real a;
 	Real b;
+	a = 500;
+	b = 200;
+	assert ((a+b)/2 == 350);
 	return 0;
 }
